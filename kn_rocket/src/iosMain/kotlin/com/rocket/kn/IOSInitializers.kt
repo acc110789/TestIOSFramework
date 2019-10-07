c61d3509b30415f585626463e49b50cf1605e7e8
zhangxiaolong <zhangxiaolong.android@bytedance.com>
2019-10-07 18:02:03 +0800

    tmp commit
    tmp commit
    
    tmp commit
    
    tmp commit
    
    tmp commit
    
    tmp commit
    
    tmp commit
    
    tmp commit

--- kn_rocket/src/iosMain/kotlin/com/rocket/kn/IOSInitializers.kt
+++ kn_rocket/src/iosMain/kotlin/com/rocket/kn/IOSInitializers.kt
@@ -7,4 +7,14 @@ object IOSInitializers {
 
     fun init() {
     }
+
+    fun getHelloWorld(): String {
+//        val time: Long = KNSystemClock.currentTimeMillis()
+        val time: Long = 10L
+        if(time % 2 == 0L) {
+            return "hello, world 1"
+        } else {
+            return "hello, world 2"
+        }
+    }
 }
