16c04d9b83af9d90977baf49048bcb62a8fce28f
zhangxiaolong <zhangxiaolong.android@bytedance.com>
2019-10-15 17:26:42 +0800

    tmp commit

--- kn_bussiness/src/iosMain/kotlin/me/test/RocketToastUtil.kt
+++ kn_bussiness/src/iosMain/kotlin/me/test/RocketToastUtil.kt
@@ -1,17 +1,11 @@
 package me.test
 
-import kn.foundation.bd.IMRIndicatorView
-import kn.foundation.bd.IMRIndicatorViewStyleImageShort
-import kn.foundation.bd.ImageAssets
+import kn.foundation.ios.applog.BoltsFrameworkVersionString
+
 
 actual object RocketToastUtil {
     actual fun showToastWithSuccessIcon(message: String) {
-        IMRIndicatorView.showWithIndicatorStyle(
-            IMRIndicatorViewStyleImageShort,
-            message,
-            ImageAssets.imr_podBundleImageWithImageName("iconToastOk24C9"),
-            true,
-            null
-        )
+        val myStr = BoltsFrameworkVersionString
+        println("myStr: $myStr")
     }
 }
