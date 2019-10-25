bf7195db87069c9135041fa58f04914acab2e2b7
zhangxiaolong <zhangxiaolong.android@bytedance.com>
2019-10-25 10:41:45 +0800

    tmp commit

--- kn_bussiness@1/src/iosMain/kotlin/me/test/RocketToastUtil.kt
+++ kn_bussiness@1/src/iosMain/kotlin/me/test/RocketToastUtil.kt
@@ -1,11 +1,8 @@
 package me.test
 
-import kn.foundation.ios.applog.BoltsFrameworkVersionString
 
 
 actual object RocketToastUtil {
     actual fun showToastWithSuccessIcon(message: String) {
-        val myStr = BoltsFrameworkVersionString
-        println("myStr: $myStr")
     }
 }
